a6754f537bd4cfcb95278c758acbd398020718f0
Bui Minh Phung <phungbminh@gmail.com>
2025-06-30 15:02:09 +0700

    fix code

--- extraction/datasets.py
+++ extraction/datasets.py
@@ -6,23 +6,45 @@ import torch
 from torch.utils.data import Dataset
 from torch_geometric.data import Data, Batch
 from collections import OrderedDict
-from utils import collate_pyg
+
+#####################
+# 1. COLLATE FN
+#####################
+
+def collate_pyg(batch):
+    """
+    Chuẩn hóa batch cho PyG:
+    batch: list of dict {'graph', 'relation', 'neg_graphs'}
+    """
+    pos_graphs = []
+    neg_graphs_list = []
+    relations = []
+    max_num_negs = max(len(item['neg_graphs']) for item in batch)
+
+    for item in batch:
+        pos_graphs.append(item['graph'])
+        relations.append(item['relation'])
+        negs = item['neg_graphs']
+        # Pad cho đều
+        if len(negs) < max_num_negs:
+            negs = negs + [Data()] * (max_num_negs - len(negs))
+        neg_graphs_list.append(negs)
+
+    return pos_graphs, relations, neg_graphs_list, max_num_negs
+
+#####################
+# 2. DATASET
+#####################
 
 class SubGraphDataset(Dataset):
-    """
-    Dataset cho LMDB subgraph.
-    - Tự động cache sample để tăng tốc truy cập.
-    - Hỗ trợ negative sampling động nếu cần.
-    - Tái sử dụng global graph để truy xuất cạnh subgraph.
-    """
     def __init__(self, db_path, mapping_dir, global_graph=None, num_negatives=0, split='train', cache_size=4096):
-        # Nạp mapping entity/relation
+        # Load entity/relation mapping
         self.entity2id = pickle.load(open(os.path.join(mapping_dir, 'entity2id.pkl'), 'rb'))
         self.relation2id = pickle.load(open(os.path.join(mapping_dir, 'relation2id.pkl'), 'rb'))
         self.id2entity = pickle.load(open(os.path.join(mapping_dir, 'id2entity.pkl'), 'rb'))
         self.id2relation = pickle.load(open(os.path.join(mapping_dir, 'id2relation.pkl'), 'rb'))
 
-        # LMDB: luôn lưu keys dạng bytes!
+        # LMDB keys
         self.env = lmdb.open(db_path, readonly=True, lock=False, readahead=False)
         with self.env.begin() as txn:
             self.keys = [k for k, _ in txn.cursor() if k != b'_progress']
@@ -32,7 +54,6 @@ class SubGraphDataset(Dataset):
         self.split = split
         self.cache = OrderedDict()
         self.cache_size = cache_size
-
         self.valid_entity_ids = set(self.entity2id.keys())
         print(f"[INFO] Loaded {len(self)} subgraphs from {db_path}")
 
@@ -41,7 +62,6 @@ class SubGraphDataset(Dataset):
 
     def __getitem__(self, idx):
         if idx in self.cache:
-            # LRU cache: đưa lên cuối
             item = self.cache.pop(idx)
             self.cache[idx] = item
             return item
@@ -55,7 +75,7 @@ class SubGraphDataset(Dataset):
         data = pickle.loads(raw)
         graph = self.create_graph(data['triple'], data['nodes'], data['s_dist'], data['t_dist'])
 
-        # Sinh negative sample (nếu training và num_negatives > 0)
+        # Sinh negative sample nếu cần
         neg_graphs = []
         if self.num_negatives > 0 and self.split == "train":
             neg_graphs = self.sample_negatives(data['triple'], data['nodes'], data['s_dist'], data['t_dist'])
@@ -67,18 +87,15 @@ class SubGraphDataset(Dataset):
         return item
 
     def create_graph(self, triple, nodes, s_dist, t_dist):
-        """Tạo PyG Data object cho subgraph"""
+        """
+        Tạo PyG Data object cho subgraph
+        """
         h_raw, r_raw, t_raw = triple
         h = self.entity2id.get(h_raw, -1)
         r = self.relation2id.get(r_raw, -1)
         t = self.entity2id.get(t_raw, -1)
-
-        # Map node ids, nếu không có thì -1
         nodes_id = [self.entity2id.get(n, -1) for n in nodes]
 
-        # Tạo mask cho head/tail
-        # head_mask = torch.tensor([n == h_raw for n in nodes], dtype=torch.bool)
-        # tail_mask = torch.tensor([n == t_raw for n in nodes], dtype=torch.bool)
         head_mask = torch.tensor(np.array(nodes) == h_raw, dtype=torch.bool)
         tail_mask = torch.tensor(np.array(nodes) == t_raw, dtype=torch.bool)
         head_idx = head_mask.nonzero(as_tuple=True)[0].item() if head_mask.any() else -1
@@ -109,9 +126,8 @@ class SubGraphDataset(Dataset):
             if node not in self.valid_entity_ids:
                 continue
             n_id = self.entity2id[node]
-            # FIX: kiểm tra chỉ số hợp lệ
             if n_id < 0 or n_id + 1 >= len(self.global_graph.indptr):
-                continue  # Bỏ node này, tránh out of bounds
+                continue  # out of bounds
             start = self.global_graph.indptr[n_id]
             end = self.global_graph.indptr[n_id + 1]
             neighbors = self.global_graph.indices[start:end]
@@ -125,19 +141,19 @@ class SubGraphDataset(Dataset):
         return edge_index
 
     def sample_negatives(self, triple, nodes, s_dist, t_dist):
-        """Negative sampling: sinh negative triples, tái sử dụng structure như positive."""
+        """
+        Sinh negative triples, tái sử dụng structure như positive.
+        """
         h_raw, r_raw, t_raw = triple
         all_entities = list(self.entity2id.keys())
         negatives = []
         for _ in range(self.num_negatives):
             if np.random.rand() < 0.5:
-                # Negative head
                 while True:
                     h_neg = np.random.choice(all_entities)
                     if h_neg != h_raw: break
                 neg_triple = (h_neg, r_raw, t_raw)
             else:
-                # Negative tail
                 while True:
                     t_neg = np.random.choice(all_entities)
                     if t_neg != t_raw: break
@@ -145,25 +161,33 @@ class SubGraphDataset(Dataset):
             negatives.append(self.create_graph(neg_triple, nodes, s_dist, t_dist))
         return negatives
 
-# --- Demo (chạy thử nghiệm nhỏ) ---
+#####################
+# 3. DEMO TEST LOADER
+#####################
+
 if __name__ == "__main__":
+    # Dummy global graph structure cho test nhanh
     class DummyCSR:
         def __init__(self, num_nodes=200):
             self.indptr = np.arange(0, num_nodes+1)
             self.indices = np.arange(num_nodes)
 
-
     dataset = SubGraphDataset(
-        db_path="/Users/minhbui/Personal/Project/Master/rel-aware-subgraph/debug_subgraph_db/train.lmdb/",
-        mapping_dir="/Users/minhbui/Personal/Project/Master/rel-aware-subgraph/debug_subgraph_db/",
+        db_path="/Users/minhbui/Personal/Project/Master/rel-aware-subgraph/debug_subgraph_db/train.lmdb",
+        mapping_dir="/Users/minhbui/Personal/Project/Master/rel-aware-subgraph/debug_subgraph_db/mappings/",
         global_graph=DummyCSR(),
         num_negatives=2,
         split='train'
     )
+
     from torch.utils.data import DataLoader
-    loader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=0, collate_fn=collate_pyg)
-    for batch_all, relations, batch_size, num_negs in loader:
-        print("Batch x:", batch_all.x.shape)
-        print("Relations:", relations.shape)
-        print("Batch_size:", batch_size, "Num negatives:", num_negs)
+    loader = DataLoader(
+        dataset, batch_size=8, shuffle=True, num_workers=0, collate_fn=collate_pyg
+    )
+
+    for pos_graphs, relations, negatives_list, num_negs in loader:
+        print("Batch size:", len(pos_graphs))
+        print("Relation IDs:", relations)
+        print("Num negatives per positive:", num_negs)
+        print("First graph x shape:", pos_graphs[0].x.shape)
         break
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -2,50 +2,52 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torch.optim import AdamW, lr_scheduler
-from tqdm import tqdm
+from tqdm.auto import tqdm
 from torch_geometric.data import Batch
+import os
+from utils import collate_pyg
 
-def create_mega_batch(pos_graphs, relations, negatives_list, num_negs):
+def create_mega_batch(pos_graphs, relations, negatives_list):
     """
-    Ghép positive và negative thành mega batch cho PyG.
+    Gộp positive và negative vào một batch PyG lớn, vectorized.
     """
-    all_graphs = []
+    all_graphs = pos_graphs + [g for negs in negatives_list for g in negs]
     all_rel = []
-    batch_size = len(pos_graphs)
-    for i in range(batch_size):
-        all_graphs.append(pos_graphs[i])
-        all_rel.append(relations[i].item() if torch.is_tensor(relations[i]) else relations[i])
-        for neg in negatives_list[i]:
-            all_graphs.append(neg)
-            all_rel.append(relations[i].item() if torch.is_tensor(relations[i]) else relations[i])
+    for i, negs in enumerate(negatives_list):
+        all_rel += [relations[i]] * (1 + len(negs))
     batch_all = Batch.from_data_list(all_graphs)
     batch_r = torch.tensor(all_rel, dtype=torch.long)
+    batch_size = len(pos_graphs)
+    num_negs = len(negatives_list[0]) if negatives_list else 0
+    return batch_all, batch_r, batch_size, num_negs
+
+def preprocess_batch(batch, device):
+    pos_graphs, relations, negatives_list, _ = batch
+    batch_all, batch_r, batch_size, num_negs = create_mega_batch(pos_graphs, relations, negatives_list)
+    # CUDA: dùng non_blocking, CPU/MPS bỏ qua param này
+    kwargs = {'non_blocking': True} if (torch.cuda.is_available() and str(device).startswith("cuda")) else {}
+    batch_all = batch_all.to(device, **kwargs)
+    batch_r = batch_r.to(device, **kwargs)
     return batch_all, batch_r, batch_size, num_negs
 
 def train_one_epoch(model, loader, optimizer, device, margin=1.0, max_grad_norm=1.0):
     model.train()
-    total_loss = 0
-    n_samples = 0
-    for pos_graphs, relations, negatives_list, num_negs in tqdm(loader, desc='Training'):
-        batch_all, batch_r, batch_size, num_negs = create_mega_batch(
-            pos_graphs, relations, negatives_list, num_negs
-        )
-        batch_all = batch_all.to(device)
-        batch_r = batch_r.to(device)
+    total_loss, n_samples = 0, 0
+    for batch in tqdm(loader, desc='Training'):
+        batch_all, batch_r, batch_size, num_negs = preprocess_batch(batch, device)
         scores = model(batch_all, batch_r).reshape(batch_size, 1 + num_negs)
-        scores_pos = scores[:, 0].unsqueeze(1)
-        scores_neg = scores[:, 1:]
+        scores_pos, scores_neg = scores[:, 0].unsqueeze(1), scores[:, 1:]
         loss = F.margin_ranking_loss(
             scores_pos.expand_as(scores_neg),
             scores_neg,
             target=torch.ones_like(scores_neg),
-            margin=margin
+            margin=margin, reduction='sum'
         )
-        optimizer.zero_grad()
+        optimizer.zero_grad(set_to_none=True)
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
         optimizer.step()
-        total_loss += loss.item() * batch_size
+        total_loss += loss.item()
         n_samples += batch_size
     return total_loss / n_samples
 
@@ -53,15 +55,10 @@ def train_one_epoch(model, loader, optimizer, device, margin=1.0, max_grad_norm=
 def evaluate(model, loader, device):
     model.eval()
     all_ranks = []
-    for pos_graphs, relations, negatives_list, num_negs in tqdm(loader, desc='Evaluating'):
-        batch_all, batch_r, batch_size, num_negs = create_mega_batch(
-            pos_graphs, relations, negatives_list, num_negs
-        )
-        batch_all = batch_all.to(device)
-        batch_r = batch_r.to(device)
+    for batch in tqdm(loader, desc='Evaluating'):
+        batch_all, batch_r, batch_size, num_negs = preprocess_batch(batch, device)
         scores = model(batch_all, batch_r).reshape(batch_size, 1 + num_negs)
-        pos_scores = scores[:, 0].unsqueeze(1)
-        neg_scores = scores[:, 1:]
+        pos_scores, neg_scores = scores[:, 0].unsqueeze(1), scores[:, 1:]
         above = (neg_scores > pos_scores).sum(dim=1)
         equal = (neg_scores == pos_scores).sum(dim=1)
         batch_ranks = 1.0 + above + equal * 0.5
@@ -84,68 +81,70 @@ def run_training(
     weight_decay=1e-5,
     patience=10,
     max_grad_norm=1.0,
-    num_workers=0,
+    num_workers=4,
     checkpoint_path=None,
-    logger=None,
+    logger=None
 ):
+    # DataLoader params auto-tuned cho CUDA/CPU
+    pin_memory = torch.cuda.is_available() and str(device).startswith("cuda")
+    loader_kwargs = dict(
+        num_workers=num_workers,
+        collate_fn=collate_pyg,
+        pin_memory=pin_memory,
+        persistent_workers=(num_workers > 0),
+        prefetch_factor=2 if num_workers > 0 else None
+    )
+
     train_loader = DataLoader(
         train_dataset,
         batch_size=batch_size,
         shuffle=True,
-        collate_fn=train_dataset.collate_fn if hasattr(train_dataset, 'collate_fn') else None,
-        num_workers=num_workers,
-        pin_memory=True
+        **loader_kwargs
     )
     valid_loader = DataLoader(
         valid_dataset,
-        batch_size=min(batch_size*4, 256),
+        batch_size=min(batch_size * 4, 256),
         shuffle=False,
-        collate_fn=valid_dataset.collate_fn if hasattr(valid_dataset, 'collate_fn') else None,
-        num_workers=num_workers
+        **loader_kwargs
     )
     test_loader = DataLoader(
         test_dataset,
-        batch_size=min(batch_size*4, 256),
+        batch_size=min(batch_size * 4, 256),
         shuffle=False,
-        collate_fn=test_dataset.collate_fn if hasattr(test_dataset, 'collate_fn') else None,
-        num_workers=num_workers
+        **loader_kwargs
     )
-
-    # Multi-GPU nếu có nhiều GPU
-    if torch.cuda.device_count() > 1 and device.startswith("cuda"):
+    # Multi-GPU support (2 card trở lên)
+    if torch.cuda.device_count() > 1 and str(device).startswith("cuda"):
         model = torch.nn.DataParallel(model)
-        if logger: logger.info(f"Using {torch.cuda.device_count()} GPUs!")
-        else: print(f"Using {torch.cuda.device_count()} GPUs!")
+        print(f"Using {torch.cuda.device_count()} GPUs with DataParallel!")
     model = model.to(device)
-    optimizer = AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+
+    # Optimizer: fused chỉ dùng được khi CUDA, torch>=2.0, nên kiểm tra an toàn
+    try:
+        optimizer = AdamW(model.parameters(), lr=lr, weight_decay=weight_decay, fused=pin_memory)
+    except TypeError:
+        optimizer = AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+
     scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=patience//2, factor=0.5)
     best_mrr, no_improve = 0, 0
     history = {'train_loss': [], 'val_mrr': [], 'val_hits': []}
 
-    for epoch in range(1, epochs+1):
+    for epoch in range(1, epochs + 1):
         msg = f"\n=== Epoch {epoch}/{epochs} ==="
-        if logger: logger.info(msg)
-        else: print(msg)
-        train_loss = train_one_epoch(
-            model, train_loader, optimizer, device, margin, max_grad_norm
-        )
+        print(msg) if logger is None else logger.info(msg)
+        train_loss = train_one_epoch(model, train_loader, optimizer, device, margin, max_grad_norm)
         history['train_loss'].append(train_loss)
         val_mrr, val_hits, _ = evaluate(model, valid_loader, device)
         history['val_mrr'].append(val_mrr)
         history['val_hits'].append(val_hits)
         scheduler.step(val_mrr)
-        msg = f"Train Loss: {train_loss:.4f} | Valid MRR: {val_mrr:.4f}"
-        if logger: logger.info(msg)
-        else: print(msg)
-        msg = f"Hits@1/3/10: {val_hits[0]:.4f}/{val_hits[1]:.4f}/{val_hits[2]:.4f}"
-        if logger: logger.info(msg)
-        else: print(msg)
-
+        msg = (f"Train Loss: {train_loss:.4f} | Valid MRR: {val_mrr:.4f} | "
+               f"Hits@1/3/10: {val_hits[0]:.4f}/{val_hits[1]:.4f}/{val_hits[2]:.4f}")
+        print(msg) if logger is None else logger.info(msg)
         if val_mrr > best_mrr:
-            best_mrr = val_mrr
-            no_improve = 0
+            best_mrr, no_improve = val_mrr, 0
             if checkpoint_path:
-                checkpoint = {
+                state = {
                     'epoch': epoch,
                     'state_dict': model.module.state_dict() if hasattr(model, 'module') else model.state_dict(),
                     'optimizer': optimizer.state_dict(),
@@ -153,32 +152,25 @@ def run_training(
                     'best_mrr': best_mrr,
                     'history': history
                 }
-                torch.save(checkpoint, checkpoint_path)
-                if logger: logger.info(f"Checkpoint saved at {checkpoint_path}")
+                torch.save(state, checkpoint_path)
+                print(f"Checkpoint saved at {checkpoint_path}") if logger is None else logger.info(f"Checkpoint saved at {checkpoint_path}")
         else:
             no_improve += 1
             if no_improve >= patience:
                 msg = f"Early stopping after {patience} epochs without improvement"
-                if logger: logger.info(msg)
-                else: print(msg)
+                print(msg) if logger is None else logger.info(msg)
                 break
 
-    # Đánh giá trên test set với model tốt nhất (nếu có checkpoint)
+    # Final test
     if checkpoint_path and os.path.exists(checkpoint_path):
-        checkpoint = torch.load(checkpoint_path, map_location=device)
-        model.load_state_dict(checkpoint['state_dict'])
-    msg = "\n==== Final Evaluation on Test Set ===="
-    if logger: logger.info(msg)
-    else: print(msg)
+        state = torch.load(checkpoint_path, map_location=device)
+        model.load_state_dict(state['state_dict'])
+        print("Loaded best model for testing") if logger is None else logger.info("Loaded best model for testing")
+
+    print("\n==== Final Evaluation on Test Set ====") if logger is None else logger.info("\n==== Final Evaluation on Test Set ====")
     test_mrr, test_hits, test_ranks = evaluate(model, test_loader, device)
-    msg = f"Test MRR: {test_mrr:.4f}, Hits@1/3/10: {test_hits[0]:.4f}/{test_hits[1]:.4f}/{test_hits[2]:.4f}"
-    if logger: logger.info(msg)
-    else: print(msg)
-    # Xuất phân phối rank
-    rank_counts = torch.bincount(test_ranks.int(), minlength=1).float()
-    rank_dist = rank_counts / rank_counts.sum()
-    if logger: logger.info("Rank Distribution: " + str(rank_dist.tolist()[:20]))
-    else: print("Rank Distribution:", rank_dist.tolist()[:20])
+    msg = (f"Test MRR: {test_mrr:.4f} | Hits@1/3/10: {test_hits[0]:.4f}/{test_hits[1]:.4f}/{test_hits[2]:.4f}")
+    print(msg) if logger is None else logger.info(msg)
     return {
         'best_val_mrr': best_mrr,
         'test_mrr': test_mrr,
